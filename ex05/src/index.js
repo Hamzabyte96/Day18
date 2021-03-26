5190f110701b748baab547a5b43791c5695ed819
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-26 07:45:02 +0100

    izmjene

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+let shoppingList = new Map([
+  [1, "Banana"],
+  [2, "Pineapple"],
+  [3, "Pear"],
+  [4, "Carrot"],
+  [5, "Apple"],
+]);
+
+let arrKey = [...shoppingList.values]
+
+let arrValue
+
+module.exports = { soppingList, arrKey, arrValue };
